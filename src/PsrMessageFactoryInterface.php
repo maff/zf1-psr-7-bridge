c642d12195798ef0979d73a39edd6f65cb9ff226
Mathias Geat <mathias@ailoo.net>
2016-12-03 00:33:01 +0100

    Define factory interfaces (mimics Symfony PSR-7 / HttpFoundation bridge)

---
+++ src/PsrMessageFactoryInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Maff\Zend1MvcPsrMessageBridge;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Creates PSR-7 requests and response from Zend Framework 1 implementations.
+ */
+interface PsrMessageFactoryInterface
+{
+    /**
+     * @param \Zend_Controller_Request_Http $request
+     * @return ServerRequestInterface
+     */
+    public function createRequest(\Zend_Controller_Request_Http $request);
+
+    /**
+     * @param \Zend_Controller_Response_Http $response
+     * @return ResponseInterface
+     */
+    public function createResponse(\Zend_Controller_Response_Http $response);
+}
---
+++ src/ZendMessageFactoryInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Maff\Zend1MvcPsrMessageBridge;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Create Zend Framework 1 requests and responses from PSR-7 implementations.
+ */
+interface ZendMessageFactoryInterface
+{
+    /**
+     * @param ServerRequestInterface $request
+     * @return \Zend_Controller_Request_Http
+     */
+    public function createRequest(ServerRequestInterface $request);
+
+    /**
+     * @param ResponseInterface $response
+     * @return \Zend_Controller_Response_Http
+     */
+    public function createResponse(ResponseInterface $response);
+}
